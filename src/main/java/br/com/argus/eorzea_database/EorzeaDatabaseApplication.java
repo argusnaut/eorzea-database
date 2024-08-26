39e400d70481757d5c9f808e932120ea4388941e
Vitor Argus <argusvi@outlook.com>
2024-08-26 10:11:24 -0300

    Initial commit

---
+++ src/main/java/br/com/argus/eorzea_database/EorzeaDatabaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.argus.eorzea_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EorzeaDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EorzeaDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/argus/eorzea_database/EorzeaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.argus.eorzea_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EorzeaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
